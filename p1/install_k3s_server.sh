294a59e40ac71a32dada17837ff3f27eac290a12
Siloze <91896400+Siloze@users.noreply.github.com>
2025-07-04 16:48:29 +0200

    Update install_k3s_server.sh

--- p1/install_k3s_server.sh
+++ p1/install_k3s_server.sh
@@ -4,6 +4,9 @@ NODE_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 export INSTALL_K3S_EXEC="--node-ip=$NODE_IP"
 
+sudo apt update && sudo apt upgrade
+sudo apt install -y curl
+
 curl -sfL https://get.k3s.io | sh -
 
 # Sauvegarde le token pour le worker
