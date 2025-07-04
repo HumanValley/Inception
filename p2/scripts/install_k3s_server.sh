7730b42438e24ee1786e2625259b36ca7cbd7fcf
Siloze <91896400+Siloze@users.noreply.github.com>
2025-07-04 16:49:09 +0200

    Update install_k3s_server.sh

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -3,5 +3,7 @@
 NODE_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 export INSTALL_K3S_EXEC="--node-ip=$NODE_IP"
+sudo apt update && sudo apt upgrade
+sudo apt install -y curl
 
 curl -sfL https://get.k3s.io | sh -
